97777525e6b4eb76a4d2a203108b62f1390f37fb
dbyrne <dbyrne@davidbyrne.site>
2015-08-14 14:35:24 -0400

    Started the ORM

---
+++ dbinit.py
@@ -0,0 +1,22 @@
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from flask_app import app
+import sqlalchemy
+
+try:
+	# db_uri = 'sqlite:///%s.db' % (app.config['DATABASE_NAME'])
+	db_uri = 'sqlite:///%s.db' % (app.config['DATABASE_NAME'])
+	conn = engine.connect()
+	conn.execute('commit')
+	# conn.execute("CREATE DATABASE " + app.config['DATABASE_NAME'])
+	conn.close()
+except:
+	print "Database exists"
+
+from flask_app import db
+
+# Add all models here
+from user.models import *
+
+db.create_all()
--- settings.py
+++ settings.py
@@ -1,2 +1,4 @@
 SECRET_KEY = 'sfsdfewfwed'
 DEBUG = True
+DATABASE_NAME = 'metmodviz'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///%s.db' % (DATABASE_NAME)
