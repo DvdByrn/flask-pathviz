2c6bbb20f2c09bdc88a9cad44082da06c4b00bfa
dbyrne <dbyrne@davidbyrne.site>
2015-08-15 07:54:46 -0400

    First decorator for login, continued

--- user/views.py
+++ user/views.py
@@ -2,24 +2,33 @@ from flask_app import app
 from flask import render_template, redirect, session, request, url_for
 from user.form import RegisterForm, LoginForm
 from user.models import User
+from user.decorators import login_required
 
 @app.route('/login', methods=('GET', 'POST'))
 def login():
     form = LoginForm()
     error = None
 
+    if request.method == 'GET' and request.args.get('next'):
+        session['next'] = request.args.get('next', None)
+
     if form.validate_on_submit():
         user = User.query.filter_by(
             username=form.username.data,
-        	password=form.password.data
+            password=form.password.data,
         ).limit(1)
         if user.count():
             session['username'] = form.username.data
-            return redirect('/login_success')
+            if 'next' in session:
+                next = session.get('next')
+                session.pop('next')
+                return redirect(next)
+            else:
+                return redirect('login_success')
         else:
             error = "Incorrect username and password"
     return render_template('user/login.html', form=form, error=error)
-
+    
 @app.route('/register', methods=('GET', 'POST'))
 def register():
     form = RegisterForm()
@@ -32,5 +41,6 @@ def success():
     return "User registered!"
 
 @app.route('/login_success')
+@login_required
 def login_success():
     return "User logged in!"
